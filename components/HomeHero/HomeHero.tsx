c627063156995961057fa29da153f205537d2652
Norbert M. Soto <57424278+nsoto92@users.noreply.github.com>
2024-03-18 23:49:31 -0400

    gallon layout

--- components/HomeHero/HomeHero.tsx
+++ components/HomeHero/HomeHero.tsx
@@ -1,7 +1,13 @@
-import { Button, Container, Title, Text } from '@mantine/core';
+'use client';
+
+import Script from 'next/script';
+import { Button, Container, Modal, Title, Text } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import styles from './HomeHero.module.css';
 
 export function HomeHero() {
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
     <div className={styles.root}>
       <Container size="lg">
@@ -36,20 +42,31 @@ export function HomeHero() {
               tu compañía de piscinas de confianza.
             </Text>
             <Button
-              component="a"
+              onClick={open}
               variant="gradient"
               gradient={{ from: 'pink', to: 'orange' }}
               size="md"
               className={styles.control}
               mt={40}
-              href="https://falconpoolsprhablaclaro.as.me/schedule/9586f425"
-              target="_blank"
             >
               Citas Aquí
             </Button>
           </div>
         </div>
       </Container>
+      <Modal opened={opened} onClose={close} size="xl">
+        <iframe
+          title="scheduler"
+          className={styles.schedulerFrame}
+          src="https://falconpoolsprhablaclaro.acuityscheduling.com"
+          width="100%"
+          height="800"
+        />
+        <Script
+          src="https://embed.acuityscheduling.com/js/embed.js"
+          type="text/javascript"
+        />
+      </Modal>
     </div>
   );
 }
